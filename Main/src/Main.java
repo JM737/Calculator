c22da87263ba270212148666b79fb0643ad1c507
JM737 <jm.billones028@gmail.com>
2025-06-30 15:22:23 +0800

    Imported a Scanner package to read the input of a user

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
 public class Main {
+    Scanner input = new Scanner(System.in);
+    /*=====================================*/
+    //  FUNCTION TO ADD TWO NUMBERS //
     static int Addition(int a, int b) {
         return a + b;
     }
-
+    //  FUNCTION TO SUBTRACT TWO NUMBERS //
     static int Subtraction(int a, int b) {
         return a - b;
     }
+    //  FUNCTION TO MULTIPLY TWO NUMBERS //
     static int Multiplication(int a, int b) {
         return a * b;
     }
+    //  FUNCTION TO DIVIDE TWO NUMBERS //
     static int Division(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Division by zero is not allowed.");
