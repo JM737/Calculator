8170201e5ebeaf8d2feb1de0a0c94ee0cb2b34cc
JM737 <jm.billones028@gmail.com>
2025-06-30 15:18:10 +0800

    Fix the code

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
-    static int Addition (int a, int b) {
+    static int Addition(int a, int b) {
         return a + b;
     }
-    static int Subtraction (int a, int b) {
+
+    static int Subtraction(int a, int b) {
         return a - b;
     }
-    //  ENTRY POINT OF THE PROGRAM //
-    public static void main (String [] args) {
 
+    //  ENTRY POINT OF THE PROGRAM //
+    public static void main(String[] args) {
+
+    }
 }
