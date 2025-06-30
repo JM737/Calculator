992524eda8f6fd6e49c5bae346ecfd2fed4ad1f2
JM737 <jm.billones028@gmail.com>
2025-06-30 15:09:35 +0800

    Add a method function named "Addition"

---
+++ Main/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    static int Addition (int a, int b) {
+        return a + b;
+    }
+    //  ENTRY POINT OF THE PROGRAM //
+    public static void main (String [] args) {
+
+    }
+}
