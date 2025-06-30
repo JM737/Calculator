5ec452030b52fba70a598d509735aac38d69e221
JM737 <jm.billones028@gmail.com>
2025-06-30 15:20:42 +0800

    Add a method a name called Division with an exception inside of it if the int b is equal to 0

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -9,6 +9,12 @@ public class Main {
     static int Multiplication(int a, int b) {
         return a * b;
     }
+    static int Division(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
     //  ENTRY POINT OF THE PROGRAM //
     public static void main(String[] args) {
 
