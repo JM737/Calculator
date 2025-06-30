2c83c857db95bfd74bbb3e921a3ba8e3d2a3736b
JM737 <jm.billones028@gmail.com>
2025-06-30 15:12:18 +0800

    Add a method function named "Addition" and "Subtraction"

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     static int Addition (int a, int b) {
         return a + b;
     }
+    static int Subtraction (int a, int b) {
+        return a - b;
+    }
     //  ENTRY POINT OF THE PROGRAM //
     public static void main (String [] args) {
 
